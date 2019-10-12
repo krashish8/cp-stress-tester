a0190a26c4bfa95be09c09e95753bde4455da050
Ashish Kumar <ashishkr23438@gmail.com>
2019-10-12 20:49:16 +0530

    Initial commit

---
+++ bad.cpp
@@ -0,0 +1,31 @@
+#include "bits/stdc++.h"
+#pragma GCC optimize "03"
+using namespace std;
+
+// #define int long long int
+#define pb push_back
+#define pii pair<int,int>
+#define fi first
+#define se second
+#define rep(i,a,b) for (int i = a; i < b; ++i)
+#define dbg(x) { cerr<<"> "<<#x<<": "<<x<< endl; }
+#define dbg2(x,y) { cerr<<"> "<<#x<<": "<<x<<" , "<<#y<<": "<<y<<endl; }
+#define dbg3(x,y,z) { cerr<<"> "<<#x<<": "<<x<<" , "<<#y<<": "<<y<<" , "<<#z<<": "<<z<<endl; }
+#define IOS ios::sync_with_stdio(false); cin.tie(0); cout.tie(0)
+#ifndef LOCAL
+#define endl '\n'
+#endif
+
+const int inf = INT_MAX;
+const int MOD = 1e9 + 7;
+const int N = 2e5 + 5;
+
+
+
+signed main(){
+    IOS;
+    int a, b;
+    cin >> a >> b;
+    cout << a + b;
+    return 0;
+}
